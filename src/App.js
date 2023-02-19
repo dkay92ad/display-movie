73ca4a6dc581190cbe9fcda514aa342004a053f7
Dkay <diwakar92ad@gmail.com>
2023-02-20 00:07:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from "react";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import ReactLoading from "react-loading";
+import { AppContainer, AppHeader } from "./styles";
+import { GlobalStyles, theme } from "common/styles/globalStyles";
+import ErrorBoundary from "common/components/ErrorBoundary";
+import ErrorPage from "common/components/ErrorPage";
+import { routes } from "common/config";
+
+const NotFound = lazy(() => import("common/components/NotFound"));
+const MovieSearch = lazy(() => import("./components/MovieSearch"));
+const Navigation = lazy(() => import("./components/Navigation"));
+const MovieFeatured = lazy(() => import("./components/MovieFeatured"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <AppContainer>
+          <AppHeader>
+            <NavLink to="/">Movie DB</NavLink>
+          </AppHeader>
+          <Navigation />
+          <ErrorBoundary fallback={<ErrorPage />}>
+            <Suspense
+              fallback={
+                <ReactLoading
+                  type="spin"
+                  color={theme.colors.primary}
+                  height={"20%"}
+                  width={"20%"}
+                />
+              }
+            >
+              <Routes>
+                <Route path={routes.HOME} element={<MovieSearch />} />
+                <Route path={routes.SEARCH} element={<MovieSearch />} />
+                <Route path={routes.FEATURED} element={<MovieFeatured />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Suspense>
+          </ErrorBoundary>
+        </AppContainer>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
